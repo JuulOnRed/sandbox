48317ecd11eed6a2ce7e9d133074aa634897e418
Matt Randell <mattrandell@Matts-MacBook-Pro.local>
2023-12-25 19:43:16 -0700

    Added new files

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Matt!");
+Console.WriteLine("This is the second line.");
+var myValue = Console.ReadLine();
+Console.WriteLine($"I typed in: {myValue}");
